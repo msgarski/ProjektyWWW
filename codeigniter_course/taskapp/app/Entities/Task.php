9d4b98767faf2268476c82588df228fe68a23321
unknown <msgarski@gmail.com>
2021-03-29 14:50:16 +0200

    cd naprawy

--- codeigniter_course/taskapp/app/Entities/Task.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Entities;
-
-class Task extends \CodeIgniter\Entity
-{
-}
--- codeigniter_course/taskapp/app/Views/Tasks/form.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <label for="description">Description</label>
-    <input type="text" name="description" id="description"
-           value="<?= old('description', esc($task->description)) ?>">
-</div>
